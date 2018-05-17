9717221a66137eaadc2205038d5db52bcc0642b8
saorisakaue <39368737+saorisakaue@users.noreply.github.com>
2018-05-17 20:11:38 +0900

    Add files via upload

---
+++ script/DB_TTD_get_all_target_genes_v180517.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+import sys
+import csv
+args = sys.argv #defining args in the command line
+
+# drug bank files
+
+DB = open("./DrugBank/pharmacologically_active.csv", 'rt')
+DB_ID = open("./DrugBank/drug_links.csv", 'rt')
+UniID = open("HUMAN_9606_idmapping.dat", 'rt')
+
+#DBID_KEGG_Commonname = {}
+DBID_Commonname = {}
+ID = csv.reader(DB_ID)
+header = next(ID)
+for row in ID:
+	DBID_Commonname[row[0]] = row[1]
+DB_ID.close()
+
+Prot_Gene = {}
+f = UniID.readlines()
+for line in f:
+	line = line.rstrip().split("\t")
+	if line[1] == "Gene_Name":
+		Prot_Gene[line[0]] = line[2] # UnipritID to gene sybbol
+UniID.close()
+
+Commonname_target = {}
+f = csv.reader(DB)
+header = next(f)
+for row in f:
+	if (row[11] == 'Human') or (row[11] == 'Homo sapiens'):
+		DBdrugs =row[12].split("; ")
+		for item in DBdrugs:
+			if row[2] != "":
+				Commonname_target.setdefault(DBID_Commonname[item].lower(),[]).append((row[2],row[0]))
+			else:
+				if row[0] in Prot_Gene.keys():
+					Commonname_target.setdefault(DBID_Commonname[item][0].lower(),[]).append((Prot_Gene[row[0]],row[0]))
+DB.close()
+
+# TTD files
+DL = open("./TTD/P1-01-TTD_download.txt", 'rt')
+Uni = open("./TTD/P2-01-TTD_uniprot_all.txt", 'rt')
+Uni_prev = open("./TTD/TTD_uniprot_success.txt", 'rt')
+
+TTD_Uniprot = {}
+TName_Uniprot_prev = {}
+
+f = Uni.readlines()
+for line in f:
+	line = line.rstrip().split("\t")
+	uniprot = line[3].split(" ")[0]
+	TTD_Uniprot.setdefault(line[0],[]).append(uniprot)
+Uni.close()
+
+f = Uni_prev.readlines()
+for line in f:
+	line = line.rstrip().split("\t")
+	targetname = line[2].lower()
+	uniprot = line[3]
+	TName_Uniprot_prev.setdefault(targetname,[]).append(uniprot)
+Uni_prev.close()
+
+TTD_targetname = {}
+f = DL.readlines()
+for line in f:
+	line = line.rstrip().split("\t")
+	if line[1] == "Name":
+		TTD_targetname[line[0]] = line[2].lower()
+	if line[1] == "Drug(s)":
+		if line[0] in TTD_Uniprot:
+			target_uniprot = TTD_Uniprot[line[0]]
+			for prot in target_uniprot:
+				if prot in Prot_Gene:
+					Commonname_target.setdefault(line[2].lower(), []).append((Prot_Gene[prot],prot))
+		else:
+			if line[0] in TTD_targetname:
+				targetname = TTD_targetname[line[0]]
+				if targetname in TName_Uniprot_prev:
+					for prot in TName_Uniprot_prev[targetname]:
+						if prot in Prot_Gene:
+							Commonname_target.setdefault(line[2].lower(), []).append((Prot_Gene[prot],prot))
+DL.close()
+
+# remove duplicate
+for drug_name in Commonname_target.keys():
+	Commonname_target[drug_name] = list(set(Commonname_target[drug_name]))
+# get all gene names
+all_genes = []
+for drug_name in Commonname_target.keys():
+	for target in Commonname_target[drug_name]:
+		all_genes.append(target[0])
+all_genes = list(set(all_genes))
+all_genes.sort()
+OUT = open("DrugBank_TTD_target_genelist.txt","a")
+for gene in all_genes:
+	print(gene, file = OUT)
---
+++ script/GDRUG_main.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+import scipy.stats
+import math
+import argparse
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--genelist','-g',default=None, type=str,
+    help='Filename of gene list to analyze.',
+	required = True
+	)
+parser.add_argument('--out','-o',default=None, type=str,
+    help='Output filename prefix.',
+	required = True
+	)
+parser.add_argument('--all','-a',default='', type=str,
+    help='A list of all genes in the scope of your analysis if available.',
+	required = False
+	)
+parser.add_argument('--test','-t',default=None, type=str,
+    help='Test mode : select from ATC or ICD.',
+	required = True
+	)
+args = parser.parse_args()
+IN = open(args.genelist,"r")
+
+target_gene = []
+f = IN.readlines()
+for line in f:
+    line = line.rstrip()
+    target_gene.append(line)
+target_gene = list(set(target_gene))
+IN.close()
+
+all_genes = []
+ALL = open("./data/DrugBank_TTD_target_genelist.txt","r")
+f = ALL.readlines()
+for line in f:
+    line = line.rstrip()
+    all_genes.append(line)
+ALL.close()
+
+if args.all != '':
+    all_defined = []
+    ALL_File = open(args.all,"r")
+    f = ALL_File.readlines()
+    for line in f:
+        line = line.rstrip()
+        all_defined.append(line)
+    all_defined = list(set(all_defined))
+    all_genes = [gene for gene in all_genes if gene in all_defined]
+    ALL_File.close()
+all_genes = list(set(all_genes))
+
+if args.test == "ATC":
+    OUT1 = open(args.out+".ATC.large.txt","a")
+    OUT2 = open(args.out+".ATC.detail.txt","a")
+    ATC_targetgene = {}
+    ATC = open("./data/DrugBank_TTD_targets_by_ATC_v2.txt")
+    f = ATC.readlines()
+    for line in f:
+        line = line.rstrip().split("\t")
+        if line[2] != "NA":
+            ATC_targetgene.setdefault(line[0], []).append(line[2])
+    ATC.close()
+    largeATC_targetgene = {}
+    for atc in ATC_targetgene.keys():
+        large_group = atc[0]
+        for gene in ATC_targetgene[atc]:
+            largeATC_targetgene.setdefault(large_group, []).append(gene)
+    target_analysis_genes = [gene for gene in target_gene if gene in all_genes]
+    target_analysis_genes = list(set(target_analysis_genes))
+    # analysis for large group
+    print("#Group\tOddsRatio\tFisherExactP",file = OUT1)
+    largegroup = list(largeATC_targetgene.keys())
+    largegroup.sort()
+    for group in largegroup:
+        this_group_genes = list(set(largeATC_targetgene[group]))
+        joined_genes = [gene for gene in this_group_genes if gene in target_gene]
+        joined_genes = list(set(joined_genes))
+        classified_genes = [gene for gene in this_group_genes if gene in all_genes]
+        classified_genes = list(set(classified_genes))
+        a = len(joined_genes)
+        b = len(classified_genes) - a
+        c = len(target_analysis_genes) - a
+        d = len(all_genes) - len(target_analysis_genes) - b
+        oddsratio, pvalue = scipy.stats.fisher_exact([[a, b], [c, d]],alternative="greater")
+        print("{0}\t{1}\t{2}".format(group,oddsratio, pvalue),file = OUT1)
+    # analysis for detailed group
+    print("#Group\tOddsRatio\tFisherExactP",file = OUT2)
+    detailedgroup = list(ATC_targetgene.keys())
+    detailedgroup.sort()
+    for group in detailedgroup:
+        this_group_genes = list(set(ATC_targetgene[group]))
+        joined_genes = [gene for gene in this_group_genes if gene in target_gene]
+        classified_genes = [gene for gene in this_group_genes if gene in all_genes]
+        a = len(joined_genes)
+        b = len(classified_genes) - a
+        c = len(target_analysis_genes) - a
+        d = len(all_genes) - len(target_analysis_genes) - b
+        oddsratio, pvalue = scipy.stats.fisher_exact([[a, b], [c, d]],alternative="greater")
+        print("{0}\t{1}\t{2}".format(group,oddsratio, pvalue),file = OUT2)
+    OUT1.close()
+    OUT2.close()
